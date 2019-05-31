1e965715089bd535632639aeb2cedbb7d50bbc9b
SANJIEMOFANG <49061775+SANJIEMOFANG@users.noreply.github.com>
2019-05-31 15:57:48 +0800

    Update mysql_install.sh

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,20 +1,79 @@
-#!/bin/bash
-#########MySQL5.7RPM auto install###########
-#########2019-5-2###########################
-mkdir -p /root/mysql
-cd /root/mysql
-wget https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.26-1.el7.x86_64.rpm-bundle.tar &>/dev/null
-tar xf mysql-5.7.26-1.el7.x86_64.rpm-bundle.tar
-#rpm -qa |grep mariadb
-yum remove mariadb-libs-5.5.56-2.el7.x86_64 &>/dev/null
-if [ $? -eq 0 ];then
-yum install libaio -y &>/dev/null
+#!/bin/sh
+if [ -s /etc/my.cnf ];then
+rm -rf /etc/my.cnf
 fi
-rpm -ivh mysql-community-common-5.7.26-1.el7.x86_64.rpm &>/dev/null
-rpm -ivh mysql-community-libs-5.7.26-1.el7.x86_64.rpm &>/dev/null
-rpm -ivh mysql-community-devel-5.7.26-1.el7.x86_64.rpm &>/dev/null
-rpm -ivh mysql-community-client-5.7.26-1.el7.x86_64.rpm &>/dev/null
-rpm -ivh mysql-community-server-5.7.26-1.el7.x86_64.rpm &>/dev/null
-rpm -ivh mysql-community-libs-compat-5.7.26-1.el7.x86_64.rpm &>/dev/null
-yum install postfix -y &>/dev/null
-systemctl start mysqld && systemctl start postfix
+
+echo "----------------------------------start install mysql -----------------------------"
+yum install -y ncurses gcc gcc-c++ ncurses ncurses-devel openssl openssl-devel libtool* cmake
+mkdir -p /data/mysql
+if [ 'grep "mysql" /etc/passwd | wc -l' ]; then
+echo "adding user mysql"
+groupadd mysql
+useradd -s /sbin/nologin -M -g mysql mysql
+else
+echo "mysql user exists"
+fi
+
+echo "-------------------------------downloading mysql----------------------------------"
+wget http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.46.tar.gz 
+
+echo "------------------------------unpackaging mysql -----------------------------------"
+tar -xvf mysql-5.5.46.tar.gz 
+cd mysql-5.5.46 
+
+echo "-------------------------configuring mysql,please wait-----------------"
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
+-DMYSQL_UNIX_ADDR=/tmp/mysql.sock \
+-DDEFAULT_CHARSET=utf8 \
+-DDEFAULT_COLLATION=utf8_general_ci \
+-DWITH_EXTRA_CHARSETS:STRING=utf8,gbk \
+-DWITH_MYISAM_STORAGE_ENGINE=1 \
+-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+-DWITH_MEMORY_STORAGE_ENGINE=1 \
+-DWITH_READLINE=1 \
+-DENABLED_LOCAL_INFILE=1 \
+-DMYSQL_DATADIR=/var/mysql/data \
+-DMYSQL_USER=mysql
+
+if [ $? -ne 0 ];then
+echo "configure failed ,please check it out!"
+exit 1
+fi
+
+echo "make mysql, please wait for 20 minutes"
+make
+if [ $? -ne 0 ];then
+echo "make failed ,please check it out!"
+exit 1
+fi
+
+make install
+
+chown -R mysql:mysql /usr/local/mysql
+chown -R mysql.mysql /data/mysql/
+
+/usr/local/mysql/scripts/mysql_install_db --user=mysql --basedir=/usr/local/mysql --datadir=/data/mysql
+#chown -R mysql /usr/local/mysql/var
+chgrp -R mysql /usr/local/mysql/
+
+cp -f ./support-files/my-large.cnf /etc/my.cnf 
+sed -i 's#^thread_concurrency = 8#& \ndatadir = /data/mysql#g' /etc/my.cnf
+cp ./support-files/mysql.server /etc/rc.d/init.d/mysql
+chmod 755 /etc/init.d/mysql
+
+#chkconfig --add mysqld
+#chkconfig --level 2345 mysqld on
+ln -s /usr/local/mysql/lib/mysql /usr/lib/mysql
+ln -s /usr/local/mysql/include/mysql /usr/include/mysql
+ln -s /usr/local/mysql/bin/mysql /usr/bin/mysql
+ln -s /usr/local/mysql/bin/mysqldump /usr/bin/mysqldump
+ln -s /usr/local/mysql/bin/myisamchk /usr/bin/myisamchk
+ln -s /usr/local/mysql/bin/mysqld_safe /usr/bin/mysqld_safe
+
+echo "mysql starting"
+/usr/local/mysql/bin/mysqld --user=mysql
+if [ $? -ne 0 ];then
+echo "mysql start filed ,please check it out!"
+else
+echo "mysql start successful,congratulations!"
+fi
